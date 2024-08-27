dcae7f94a93644a7dead812aa15a956c5c453486
Abhirup Pan <abhiruppan2016@gmail.com>
2024-08-28 02:30:00 +0530

    Final Commit2

--- src/Pages/Bookacall.tsx
+++ src/Pages/Bookacall.tsx
@@ -32,7 +32,7 @@ function Bookacall() {
 
   return (
     <div className=" relative mt-[30vh] mx-[6vw] font-bold flex flex-col items-center font-Poiret">
-      <h1 className=' text-2xl font-medium underline underline-offset-[2vh] decoration-uniqueYellow cursor-default'>
+      <h1 className=' text-3xl font-medium underline underline-offset-[2vh] decoration-uniqueYellow cursor-default'>
         Quick Enquiry
       </h1>
       <p className=' my-[10vh] text-2xl'>
@@ -83,7 +83,7 @@ function Bookacall() {
         </form>
       </div>
       <div className=" relative  mt-[10vh] mb-[10vh] flex flex-col items-center">
-      <h1 className='  text-2xl font-medium underline underline-offset-[2vh] decoration-uniqueYellow cursor-default my-[10vh]'>Customer Support</h1>
+      <h1 className='  text-3xl font-medium underline underline-offset-[2vh] decoration-uniqueYellow cursor-default my-[10vh]'>Customer Support</h1>
       <div className='flex flex-col md:flex-row gap-5 '>
       <div className='flex flex-col justify-center gap-4 items-center   bg-uniqueYellow border-black border-2 rounded-br-lg rounded-bl-lg w-[55vw] h-[20vh] md:w-[26.5vw] md:h-[40vh] shadow-lg shadow-gray-500 '>
         <IconPhone className='text-base sm:text-3xl'/>
@@ -107,7 +107,7 @@ function Bookacall() {
         <p className='bg-white px-4 font-bold w-full text-center'>SH 8, Jagat Berh, Barabalidanga, Bardhaman, West Bengal 713103</p>
       </div>
       </div>
-      <h1 className='  text-2xl font-medium underline underline-offset-[2vh] decoration-uniqueYellow cursor-default my-[10vh]'>Google Maps</h1>
+      <h1 className='  text-3xl font-medium underline underline-offset-[2vh] decoration-uniqueYellow cursor-default my-[10vh]'>Google Maps</h1>
       <div className='my-[5vh]'><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3666.505052243271!2d87.855727!3d23.2247016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f8492465c7b385%3A0x97ec3993ec1b9bd0!2sSATYAM%20CONSTRUCTION!5e0!3m2!1sen!2sin!4v1724657146582!5m2!1sen!2sin" width="600" height="450" loading="lazy" ></iframe></div>
       </div>
     </div>
